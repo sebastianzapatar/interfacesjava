d1ffc3723a5ba2845b6ee0539b2907379a8e22e9
Sebastian Zapata <pansezapata@gmail.com>
2025-09-17 11:20:13 -0500

    Se crea el main para hacer pruebas

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,15 @@
-import animales.Gato;
-import animales.Mamifero;
-import animales.Perro;
+import ZOO.Zoologico;
+import animales.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        Perro perro = new Perro();
-        perro.setNombre("Perro");
-        perro.setEdad(14);
-        perro.setColor("Verde");
-        perro.setNombre("Chandrix");
-        Gato gato = new Gato();
-        Mamifero mw[]=new Mamifero[2];
-        mw[0]=perro;
-        mw[1]=gato;
-        Mamifero perrito=new Perro();
-        for(Mamifero m:mw){
-            m.morir();
-
-        }
+        Zoologico zoo = new Zoologico();
+        Animal animal = new Delfin("Winter",10);
+        zoo.agregarAnimal(animal);
+        Animal animal2 = new Loro("Sebastian",36);
+        zoo.agregarAnimal(animal2);
+        zoo.mostrarSonidos();
+        zoo.mostrarHabilidad();
 
     }
 }
